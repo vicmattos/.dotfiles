02fd19eea76d0b971ab4cf7872af732312d2eabe
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2024-01-25 15:27:35 -0300

    feat: add private key permission setting in ssh setup

--- homebrew/.config/homebrew/setup/ssh.zsh
+++ homebrew/.config/homebrew/setup/ssh.zsh
@@ -8,6 +8,8 @@ eval $(ssh-agent -s)
 if [ ! -f ~/.ssh/id_ed25519 ]; then
     echo 'private key not found in ~/.ssh/id_ed25519'
 else
+    echo 'ensure 400 permission for ssh private key'
+    chmod 400 ~/.ssh/id_ed25519
     echo 'adding ~/.ssh/id_ed25519 in ssh-agent (needs key password)'
     ssh-add ~/.ssh/id_ed25519
 fi
