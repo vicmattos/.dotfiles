79199dbb44a374dbe47ecd0728bf8bfb9c8f72d3
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2024-10-09 16:52:02 -0300

    Refactor `.ansible/library/install.sh` to ensure installation in script directory

--- .ansible/library/install.sh
+++ .ansible/library/install.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-curl https://raw.githubusercontent.com/caian-org/ansible-stow/v0.1.3/stow -o stow
+
+BASEDIR=$(dirname "$0")
+
+curl https://raw.githubusercontent.com/caian-org/ansible-stow/v0.1.3/stow -o ${BASEDIR}/stow
